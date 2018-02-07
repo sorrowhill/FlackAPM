f8be3215a1b937567760d60f122f65edc83e290b
sorrowhill <sorrowhill00@gmail.com>
2018-02-07 20:14:59 +0900

    initial commit

---
+++ run.py
@@ -0,0 +1,53 @@
+import newrelic.agent
+newrelic.agent.initialize('./newrelic.ini')
+
+from flask import Flask
+app = Flask(__name__)
+app = newrelic.agent.wsgi_application()(app)
+
+from time import sleep
+import logging
+import random
+
+from threading import Timer
+
+
+@newrelic.agent.background_task()
+def background_task():
+    while True:
+        value = random.randrange(1, 100000)
+        if value <= 50:
+            value_dict = {'value': value}
+            newrelic.agent.record_custom_event("my_event", value_dict)
+            break
+
+    Timer(1, background_task).start()
+
+
+@app.route('/test1')
+def run_test1():
+    newrelic.agent.set_transaction_name("test1")
+    sleep(1)
+    return "done\n"
+
+
+@app.route('/test2')
+def run_test2():
+    newrelic.agent.set_transaction_name("test2")
+    sleep(2)
+    return "done\n"
+
+
+@app.route('/test3')
+def run_test3():
+    newrelic.agent.set_transaction_name("test3")
+    sleep(3)
+    return "done\n"
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+    Timer(1, background_task).start()
+
+    app.run()
---
+++ run_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+  curl http://127.0.0.1:5000/test1
+  curl http://127.0.0.1:5000/test2
+  curl http://127.0.0.1:5000/test3
+done
